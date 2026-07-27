7b08662e7ebfd97da462548e948be8375f902580
Khoa-tech <dubakhoa@gmail.com>
2026-07-27 12:56:18 +0700

    Update environment.h

--- src/harness/environment.h
+++ src/harness/environment.h
@@ -1,1 +1,26 @@
 // Placeholder: abstract Environment interface.
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+class Environment {
+public:
+    virtual ~Environment() = default;
+
+    virtual void setup() = 0;
+
+    virtual void cleanup() = 0;
+};
+
+class NativeEnvironment : public Environment {
+public:
+    void setup() override {
+        // Có thể tạo thư mục output,
+        // xóa kết quả cũ hoặc chuẩn bị file test.
+    }
+
+    void cleanup() override {
+        // Dọn tài nguyên sau khi chạy task.
+    }
+};
+
+#endif
