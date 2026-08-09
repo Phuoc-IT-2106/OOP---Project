5af4ceb4957f1232d995176106a2869eacbae201
Khoa-tech <dubakhoa@gmail.com>
2026-08-09 17:02:41 +0700

    Add GUI action structures and variant type

--- src/gui/gui_action.h
+++ src/gui/gui_action.h
@@ -1,1 +1,39 @@
+#pragma once
 
+#include <string>
+#include <variant>
+
+namespace oop_agent::gui {
+
+struct ClickAction {
+    int x{0};
+    int y{0};
+    int button{1};
+};
+
+struct TypeTextAction {
+    std::string text;
+};
+
+struct KeyPressAction {
+    std::string key;
+};
+
+struct MoveMouseAction {
+    int x{0};
+    int y{0};
+};
+
+struct DoneAction {
+    std::string message;
+};
+
+using GuiAction = std::variant<
+    ClickAction,
+    TypeTextAction,
+    KeyPressAction,
+    MoveMouseAction,
+    DoneAction
+>;
+
+} // namespace oop_agent::gui
